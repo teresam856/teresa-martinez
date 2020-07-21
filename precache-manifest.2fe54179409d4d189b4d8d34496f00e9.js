9a2aa5e1f23b19ba4a53cb7969e17b3919aa18b7
teresam856 <tere486martinez@gmail.com>
2020-07-20 21:47:34 -0700

    Updates

---
+++ precache-manifest.2fe54179409d4d189b4d8d34496f00e9.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e60fddc0c899e8aadaaccd1e59d7e30b",
+    "url": "/teresa-martinez/index.html"
+  },
+  {
+    "revision": "90abed3070e10104d4a9",
+    "url": "/teresa-martinez/static/css/main.8af8ef0c.chunk.css"
+  },
+  {
+    "revision": "a0c6f0e1b28adeff6fc7",
+    "url": "/teresa-martinez/static/js/2.b6b998ef.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/teresa-martinez/static/js/2.b6b998ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90abed3070e10104d4a9",
+    "url": "/teresa-martinez/static/js/main.0cc49c0c.chunk.js"
+  },
+  {
+    "revision": "bf678ff126ec202e4b96",
+    "url": "/teresa-martinez/static/js/runtime-main.7f4c5641.js"
+  },
+  {
+    "revision": "2772ecb3f33420476aa9dcbe7cf01cb2",
+    "url": "/teresa-martinez/static/media/berkeley.2772ecb3.png"
+  },
+  {
+    "revision": "3e67717a78b8063afb621e7856b29e2f",
+    "url": "/teresa-martinez/static/media/berkeley3.3e67717a.png"
+  },
+  {
+    "revision": "1cc644e929c36f2f33504c14a4591301",
+    "url": "/teresa-martinez/static/media/me.1cc644e9.png"
+  },
+  {
+    "revision": "0b26e7b57c4453344bf442b3ae36b463",
+    "url": "/teresa-martinez/static/media/me3.0b26e7b5.png"
+  },
+  {
+    "revision": "17d4e0f219e421bf1a638bb3e11bbaf9",
+    "url": "/teresa-martinez/static/media/ny.17d4e0f2.png"
+  },
+  {
+    "revision": "0b8d76e0a0729df323d3871d9b0cb9ed",
+    "url": "/teresa-martinez/static/media/seattle.0b8d76e0.png"
+  },
+  {
+    "revision": "33902835250f69724891519eccb521c9",
+    "url": "/teresa-martinez/static/media/sf.33902835.png"
+  },
+  {
+    "revision": "821a59306926735310fa543192017c17",
+    "url": "/teresa-martinez/static/media/sky.821a5930.png"
+  },
+  {
+    "revision": "0eeb8265d9bedbdf2d5db0113c36f793",
+    "url": "/teresa-martinez/static/media/sunflowers.0eeb8265.png"
+  },
+  {
+    "revision": "79f038b6f7334dc69259f9baf263e5b6",
+    "url": "/teresa-martinez/static/media/sunset.79f038b6.png"
+  },
+  {
+    "revision": "ae8e41939888e2ad58edbcec637890b3",
+    "url": "/teresa-martinez/static/media/wmall.ae8e4193.png"
+  }
+]);
